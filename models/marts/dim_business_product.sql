9705cb6c64e592d196ee275180063ea4c0be14a7
Santi Abruzzini <abruzzini.s@gmail.com>
2022-07-28 13:59:17 +0000

    varios cambios en models, sources y tests

--- models/marts/dim_business_product.sql
+++ models/marts/dim_business_product.sql
@@ -1,10 +1,10 @@
-select id as business_product_id,
-       status,
+select id as product_id,
+       upper(status) as product_status,
        version,
        created_at,
        updated_at,
        _fivetran_deleted,
        _fivetran_synced,
        business_id,
-       product_name
+       upper(product_name) as product_name
 from {{ source('mundi', 'BUSINESS_PRODUCT') }}
--- tests/assert_total_payment_amount_is_positive.sql
+++
@@ -1,5 +0,0 @@
-select invoice_advance_id,
-sum(total)
-from {{ source('mundi', 'INVOICE_ADVANCE') }}
-where total >= 0
-group by 1
