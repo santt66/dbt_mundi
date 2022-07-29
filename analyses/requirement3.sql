dd8673b55309dd2fe46819a211970e026a132bec
Santi Abruzzini <abruzzini.s@gmail.com>
2022-07-29 00:12:16 +0000

    cambios varios en requerimientos

--- analyses/requirement3.sql
+++ analyses/requirement3.sql
@@ -1,19 +1,20 @@
+-- REQUERIMIENTO 3
 -- Listado de TODOS los business_product de tipo Invoice Advance que
 -- NO tengan invoice_advance o que los que tiene sean todos rejected. Solo considerar los invoice advance en USD
 
---1ro filtré los invoices rejected:
+-- 1ro filtré los invoices rejected:
 with rejected_invoice_advance as (SELECT *
                                   FROM MUNDI.PUBLIC.FCT_INVOICE_ADVANCE
                                   WHERE CURRENCY = 'USD'
                                     and INVOICE_ADVANCE_STATE = 'REJECTED'),
 
---2do hice un listado de business_product_ids únicos con estado válido de la tabla invoice_advance
+-- 2do hice un listado de business_product_ids únicos con estado válido de la tabla invoice_advance
      distinct_product_ids AS (SELECT DISTINCT BUSINESS_PRODUCT_ID
                               FROM MUNDI.PUBLIC.FCT_INVOICE_ADVANCE
                               WHERE CURRENCY = 'USD'
                                 AND INVOICE_ADVANCE_STATE <> 'REJECTED')
 
---3ro: unifiqué las subqueries a la tabla business product
+-- 3ro: unifiqué las subqueries a la tabla business product
 select a.PRODUCT_ID,
        case when c.BUSINESS_PRODUCT_ID is null then 'No Valid Invoices' else 'Has Valid Invoices' end as has_invoices,
        count(b.INVOICE_ADVANCE_ID)                                                                    as rejected
@@ -23,3 +24,5 @@ from MUNDI.PUBLIC.DIM_BUSINESS_PRODUCT a
 where has_invoices = 'No Valid Invoices'
 group by 1, 2
 order by 2 desc, 3 desc
+
+-- Hay entonces 53 products IDs que no han emitido facturas válidas en USD; 52 de ellos nunca solicitaron adelanto y 1 de ellos tiene 168 adelantos rechazados.
